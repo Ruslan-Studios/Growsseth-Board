09c6f39ae6fdf576bb73d1dfbcb83abdf8a11e58
Noel <leonardoulino2005@gmail.com>
2024-05-20 19:13:28 +0200

    Clamp zoom-in-out camera

--- scenes/MapCamera.gd
+++ scenes/MapCamera.gd
@@ -23,3 +23,6 @@ func _input(event):
 	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_WHEEL_UP:
 		position.z -= sensivity
 		position.y -= sensivity
+		
+	position.y = clamp(position.y, 7, 21)
+	position.z = clamp(position.z, 7, 21)
