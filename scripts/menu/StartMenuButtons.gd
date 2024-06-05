348edb19f318f03858135281b35c9f279e8d0ee5
UomoProtone <protonic.studios.pj@gmail.com>
2024-06-06 00:41:01 +0200

    Basic main and settings menus

---
+++ scripts/menu/StartMenuButtons.gd
@@ -0,0 +1,22 @@
+extends MarginContainer
+
+# Pulsanti
+@export var play: Button
+@export var settings: Button
+@export var rules: Button
+@export var credits: Button
+@export var exit: Button
+
+@export var playScene: PackedScene
+@onready var settings_menu = $"../SettingsMenu"
+
+func _on_play_pressed():
+	get_tree().change_scene_to_packed(playScene)
+
+func _on_settings_pressed():
+	settings_menu.visible = true
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
